069f361376b792bb4b11db685a25fb385c103303
Sharon <sharon.yi@me.com>
2019-11-15 11:45:02 -0500

    added gameover n sweetalert

--- script.js
+++ script.js
@@ -53,6 +53,13 @@ $(function () {
       $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) - 5);
       moveLeft = requestAnimationFrame(left);
     }
+    if (totalSeconds >= 60) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) - 12);
+    } else if (totalSeconds >= 30) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) - 6.5);
+    } else if (totalSeconds >= 10) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) - 5.5);
+    }
   };
 
   let right = function () {
@@ -61,75 +68,130 @@ $(function () {
       $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) + 5);
       moveRight = requestAnimationFrame(right);
     }
+    if (totalSeconds >= 60) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) + 12);
+    } else if (totalSeconds >= 30) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) + 6.5);
+    } else if (totalSeconds >= 10) {
+      $(".rocketArea").css("left", parseInt($(".rocketArea").css("left")) + 5.5);
+    }
   };
 
+  function gameEnds() {
+    gameOver = true;
+    requestAnimationFrame(keepFalling) = false;
+    cancelAnimationFrame(moveRight);
+    cancelAnimationFrame(moveLeft);
 
+  }
   //a request animation frame idek
+
   keepFalling = requestAnimationFrame(repeat);
 
   function repeat() {
     if (gameOver === false) {
       if (collision(rh, mh1) || collision(rh, mh2) || collision(rh, mh3) || collision(rh, mh4) || collision(rh, mw1) || collision(rh, mw2) || collision(rh, mw3) || collision(rh, mw4) || collision(rw, mh1) || collision(rw, mh2) || collision(rw, mh3) || collision(rw, mh4) || collision(rw, mw1) || collision(rw, mw2) || collision(rw, mw3) || collision(rw, mw4)) {
-        alert('you died!!! :(');
-        location.reload(true);
+        Swal.fire({
+          title: 'You lost!',
+          text: "Try again to beat it wah",
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'OKAY',
+          allowOutsideClick: false
+        }).then((result) => {
+          if (result.value) {
+            location.reload(true);
+          }
+        })
+        gameEnds();
       }
     }
-
-    meteorFalling($('.ma1'));
-    meteorFalling($('.ma2'));
-    meteorFalling($('.ma3'));
-    meteorFalling($('.ma4'));
     starFalling($('.star1'));
     starFalling($('.star2'));
     starFalling($('.star3'));
     starFalling($('.star4'));
 
+    meteorFalling($('.ma1'));
+    meteorFalling($('.ma2'));
+    meteorFalling($('.ma3'));
+    meteorFalling($('.ma4'));
 
     requestAnimationFrame(repeat);
   };
-
 
   function meteorFalling(meteor) {
     let topOfArea = parseInt(meteor.css('top'));
     if (topOfArea > parseInt($('.gameArea').css('height'))) {
       topOfArea = -100;
-      let randomPosition = parseInt(Math.random() * 320);
+      let randomPosition = parseInt(Math.random() * 360);
       meteor.css('left', randomPosition);
     }
-    meteor.css('top', topOfArea + 3);
+    meteor.css('top', topOfArea + 4);
 
     if (totalSeconds >= 60) {
       meteor.css('top', topOfArea + 15);
-    } else if (totalSeconds >= 50) {
-      meteor.css('top', topOfArea + 10);
     } else if (totalSeconds >= 40) {
-      meteor.css('top', topOfArea + 7);
+      meteor.css('top', topOfArea + 10);
     } else if (totalSeconds >= 30) {
-      meteor.css('top', topOfArea + 6);
+      meteor.css('top', topOfArea + 8);
     } else if (totalSeconds >= 20) {
-      meteor.css('top', topOfArea + 5);
+      meteor.css('top', topOfArea + 6);
     } else if (totalSeconds >= 10) {
-      meteor.css('top', topOfArea + 4);
+      meteor.css('top', topOfArea + 5);
     }
   }
-      function starFalling(star) {
-        let topOfArea = parseInt(star.css('top'));
-        if (topOfArea > parseInt($('.gameArea').css('height'))) {
-          topOfArea = 0;
-          let randomPosition = parseInt(Math.random() * 350);
-          star.css('left', randomPosition);
-        }
-        star.css('top', topOfArea + 10);
-      }
 
-  var totalSeconds = 0;
+  function starFalling(star) {
+    let topOfAreaStar = parseInt(star.css('top'));
+    if (topOfAreaStar > parseInt($('.gameArea').css('height'))) {
+      topOfAreaStar = 0;
+      let randomPositionStar = parseInt(Math.random() * 350);
+      star.css('left', randomPositionStar);
+    }
+    star.css('top', topOfAreaStar + 10);
+
+    if (totalSeconds >= 60) {
+      star.css('top', topOfAreaStar + 20);
+    } else if (totalSeconds >= 40) {
+      star.css('top', topOfAreaStar + 16);
+    } else if (totalSeconds >= 30) {
+      star.css('top', topOfAreaStar + 13);
+    } else if (totalSeconds >= 20) {
+      star.css('top', topOfAreaStar + 11);
+    }
+  }
+
+  let totalSeconds = 0;
   setInterval(setTime, 1000);
 
   function setTime() {
     ++totalSeconds;
     console.log(totalSeconds)
   }
-  
+
+
+
+
+  //FIGURE OUT THE SCORE COUNTERRRRRR
+
+  // function counter() {
+  //   var i = 0;
+  //   var funcNameHere = function () {
+  //     if (i == 100) clearInterval(this);
+  //     else console.log('Currently at ' + (i++));
+  //   };
+  //   // This block will be executed 100 times.
+  //   setInterval(funcNameHere, 7000);
+  //   funcNameHere();
+
+
+  //   var amount = document.getElementById('scoreCounter');
+  //   var current = 0;
+
+  //   setInterval(function () {
+  //     current += 5;
+  //   }, 1000);
+  //   console.log(setInterval());
 
 
 
