5a188d7d4cd2b1083fb972888cdfd001a4308b94
Sharon <sharon.yi@me.com>
2019-11-17 19:37:45 -0500

    in the process of cleaning up code :shower:

--- script.js
+++ script.js
@@ -3,31 +3,31 @@
   let moveRight = false;
   let rh = $('.rocketHeight')
   let rw = $('.rocketWidth')
-  let ma1 = $('.ma1')
-  let ma2 = $('.ma2')
-  let ma3 = $('.ma3')
-  let ma4 = $('.ma4')
-  let mh1 = $('.mh1')
-  let mh2 = $('.mh2')
-  let mh3 = $('.mh3')
-  let mh4 = $('.mh4')
-  let mw1 = $('.mw1')
-  let mw2 = $('.mw2')
-  let mw3 = $('.mw3')
-  let mw4 = $('.mw4')
+  let ma1 = $('.asteroidAreaOne')
+  let ma2 = $('.asteroidAreaTwo')
+  let ma3 = $('.asteroidAreaThree')
+  let ma4 = $('.asteroidAreaFour')
+  let mh1 = $('.asteroidHeightOne')
+  let mh2 = $('.asteroidHeightTwo')
+  let mh3 = $('.asteroidHeightThree')
+  let mh4 = $('.asteroidHeightFour')
+  let mw1 = $('.asteroidWidthOne')
+  let mw2 = $('.asteroidWidthTwo')
+  let mw3 = $('.asteroidWidthThree')
+  let mw4 = $('.asteroidWidthFour')
 
   $(function () { // getting document ready
 
     Swal.fire({ //alert for game start 
-      title: 'Hello!',
-      text: 'While you and your friend are out on a super secret and important space mission, your friend challenges you to see how far you can travel through the asteroid belt without getting hit! Use left and right arrows to dodge the asteroids!',
+      title: 'WELCOME!',
+      text: 'While you and your friend are out on a super secret and important space mission, your friend challenges you to see how far you can travel through the asteroid belt without getting hit! Use left and right arrows to dodge the asteroids',
       confirmButtonColor: '#9b2323',
       confirmButtonText: "LET'S GO",
       allowOutsideClick: false
     }).then((result) => {
       if (result.value) {
         gameOver = false;
-        // document.getElementById("audio").play();
+        document.getElementById("audio").play();
       }
 
       //MOVING LEFT AND RIGHT STARTS
@@ -162,31 +162,31 @@
         starFalling($('.star3'));
         starFalling($('.star4'));
 
-        meteorFalling($('.ma1'));
-        meteorFalling($('.ma2'));
-        meteorFalling($('.ma3'));
-        meteorFalling($('.ma4'));
+        asteroidFalling($('.asteroidAreaOne'));
+        asteroidFalling($('.asteroidAreaTwo'));
+        asteroidFalling($('.asteroidAreaThree'));
+        asteroidFalling($('.asteroidAreaFour'));
       };
 
-      function meteorFalling(meteor) {
-        let topOfArea = parseInt(meteor.css('top'));
+      function asteroidFalling(asteroid) {
+        let topOfArea = parseInt(asteroid.css('top'));
         if (topOfArea > parseInt($('.gameArea').css('height'))) {
           topOfArea = -150;
           let randomPosition = parseInt(Math.random() * 360);
-          meteor.css('left', randomPosition);
+          asteroid.css('left', randomPosition);
         }
-        meteor.css('top', topOfArea + 4);
+        asteroid.css('top', topOfArea + 4);
 
         if (totalKm >= 400) {
-          meteor.css('top', topOfArea + 10);
+          asteroid.css('top', topOfArea + 10);
         } else if (totalKm >= 320) {
-          meteor.css('top', topOfArea + 8);
+          asteroid.css('top', topOfArea + 8);
         } else if (totalKm >= 240) {
-          meteor.css('top', topOfArea + 7);
+          asteroid.css('top', topOfArea + 7);
         } else if (totalKm >= 160) {
-          meteor.css('top', topOfArea + 6);
+          asteroid.css('top', topOfArea + 6);
         } else if (totalKm >= 80) {
-          meteor.css('top', topOfArea + 5);
+          asteroid.css('top', topOfArea + 5);
         }
       }
 
@@ -210,27 +210,27 @@
         }
       }
 
-      // COLLISION CODE from https://gist.github.com/jaxxreal/7527349 :)
-
+      // [COLLISION FUNCTION CODE] from https://gist.github.com/jaxxreal/7527349 :)
+      // if div1 hits div2 then perform all these actions
       function collision($div1, $div2) {
-        var x1 = $div1.offset().left;
-        var y1 = $div1.offset().top;
-        var h1 = $div1.outerHeight(true);
-        var w1 = $div1.outerWidth(true);
-        var b1 = y1 + h1;
-        var r1 = x1 + w1;
-        var x2 = $div2.offset().left;
-        var y2 = $div2.offset().top;
-        var h2 = $div2.outerHeight(true);
-        var w2 = $div2.outerWidth(true);
-        var b2 = y2 + h2;
-        var r2 = x2 + w2;
+        let x1 = $div1.offset().left;
+        let y1 = $div1.offset().top;
+        let h1 = $div1.outerHeight(true);
+        let w1 = $div1.outerWidth(true);
+        let b1 = y1 + h1;
+        let r1 = x1 + w1;
+        let x2 = $div2.offset().left;
+        let y2 = $div2.offset().top;
+        let h2 = $div2.outerHeight(true);
+        let w2 = $div2.outerWidth(true);
+        let b2 = y2 + h2;
+        let r2 = x2 + w2;
 
         if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
         return true;
       }
     });
-
-
-
   });
+
+  // Music: Eric Skiff - Song Name - Chibi Ninja - Available at http://EricSkiff.com/music
+  // shoutout to https://developer.mozilla.org/en-US/docs/Web/JavaScript helped me with majority of my code
